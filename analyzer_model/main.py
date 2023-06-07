9fae32a7b6736f7574242b245382667c68fbc861
VISHU1999 <viveksharma4318@gmail.com>
2023-06-07 16:50:58 +0530

    Update python files

--- analyzer_model/main.py
+++ analyzer_model/main.py
@@ -1,13 +1,15 @@
+"""Main file of doc analyzer function"""
+
 import os
 
-from dotenv.main import load_dotenv
-from langchain import HuggingFaceHub
-from langchain.chains import RetrievalQA
-from langchain.document_loaders import PyPDFLoader
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.prompts import PromptTemplate
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import Chroma
+from dotenv.main import load_dotenv #pylint: disable=all
+from langchain import HuggingFaceHub #pylint: disable=all
+from langchain.chains import RetrievalQA #pylint: disable=all
+from langchain.document_loaders import PyPDFLoader #pylint: disable=all
+from langchain.embeddings import HuggingFaceEmbeddings #pylint: disable=all
+from langchain.prompts import PromptTemplate #pylint: disable=all
+from langchain.text_splitter import CharacterTextSplitter #pylint: disable=all
+from langchain.vectorstores import Chroma #pylint: disable=all
 
 load_dotenv()
 
@@ -28,23 +30,25 @@ def doc_analyzer(file_path: str, question: str):
         repo_id=model, model_kwargs={"temperature": 0, "max_length": 712}
     )
 
-    prompt_template = """Act as an Resume expert. Analyze the data in the document and answer the following question. 
-    do a good job in evaluating the person using professional questions. If you don't know the answer, just say that you don't know.
+    prompt_template = """Act as an Resume expert.
+    Analyze the data in the document and answer the following question.
+    do a good job in evaluating the person using professional questions. 
+    If you don't know the answer, just say that you don't know.
 
     Act as an Interviewer :{context}
 
     Question: {question}
     """
-    PROMPT = PromptTemplate(
+    prompt = PromptTemplate(
         template=prompt_template, input_variables=["context", "question"]
     )
-    chain_type_kwargs = {"prompt": PROMPT}
-    qa = RetrievalQA.from_chain_type(
+    chain_type_kwargs = {"prompt": prompt}
+    query = RetrievalQA.from_chain_type(
         llm=llm,
         chain_type="stuff",
         retriever=docsearch.as_retriever(search_kwargs={"k": 1}),
         chain_type_kwargs=chain_type_kwargs,
     )
 
-    result = qa.run(question)
+    result = query.run(question)
     return result
--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
-from setuptools import setup,find_packages
-import os
-with open('requirements.txt') as f:
+"""Setup file"""
+
+from setuptools import setup, find_packages
+
+with open("requirements.txt",encoding='UTF-8') as f:
     required = f.read().splitlines()
 
-setup(name='analyzer_model',
-      version='0.1',
-      description='It analyze your doc and answer your',
-      author='Boss',
-      # author_email='vi',
-      # license='MIT',
-      install_requires=required,
-      packages=find_packages(where='analyzer_model'),
-      # zip_safe=False
+setup(
+    name="analyzer_model",
+    version="0.1",
+    description="It analyze your doc and answer your",
+    author="Boss",
+    # author_email='vi',
+    # license='MIT',
+    install_requires=required,
+    packages=find_packages(where="analyzer_model"),
+    # zip_safe=False
 )
